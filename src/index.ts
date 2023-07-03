eeb21c90a33ce4a50d2df29df62e1e06f651bc90
i.bykhavets <i.bykhavets@softarex.com>
2023-07-03 20:46:03 +0200

    feat: add fake DataBase server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import { createServer, request } from "node:http";
 
 import { server } from './server/server';
+import { fakeDBServer } from "./database/fakeDateBase";
 
 import * as errors from './constants/errors';
 
@@ -20,6 +21,8 @@ let port = Number(process.env.PORT) || 4000;
 const dbPort = Number(process.env.FAKE_DB_PORT) || 4080;
 const args = process.argv;
 
+fakeDBServer.listen(dbPort);
+process.env.WORKER_PORT = '4000';
 server.listen(port, () => {
     console.log(`Server is running at PORT ${port}`);
 });
